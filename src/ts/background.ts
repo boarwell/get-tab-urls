78f0ec4637f019832c088858490b3357c492679e
boarwell <34891695+boarwell@users.noreply.github.com>
2021-07-29 02:12:34 +0900

    deleted the background script

--- src/ts/background.ts
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(async () => {
-  const tabs = await chrome.tabs.query({ pinned: false });
-  const webPageTabs = tabs.filter((tab) => tab.url?.startsWith("http"));
-
-  console.log(webPageTabs);
-});
