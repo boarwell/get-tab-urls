1cfa8a640d9c0369aa8c0ccf6bb9a54121f60f3c
boarwell <34891695+boarwell@users.noreply.github.com>
2021-07-29 01:11:45 +0900

    polish & add comments

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -1,16 +1,18 @@
-function isWebPageTab(tab: chrome.tabs.Tab): boolean {
-  if (tab.url == undefined) {
-    return false;
-  }
-
-  return tab.url.startsWith("http");
+/**
+ * Checks if the tab is opening a web page (instead of a page like chrome://*).
+ */
+function isWebPage(tab: chrome.tabs.Tab): boolean {
+  return (tab.url ?? "").startsWith("http");
 }
 
+/**
+ * Gets the url of tabs on current window (excluding pinned tabs).
+ */
 async function getPageURLs(): Promise<string[]> {
   const tabs = await chrome.tabs.query({ pinned: false, currentWindow: true });
-  const webPageTabs = tabs.filter(isWebPageTab);
+  const urls = tabs.flatMap((tab) => (isWebPage(tab) ? [tab.url ?? ""] : []));
 
-  return webPageTabs.map((tab) => tab.url ?? "").filter(Boolean);
+  return urls;
 }
 
 (async function main() {
